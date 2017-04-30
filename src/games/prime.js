5328b7bccdce769a7b6e7fe2c614588b6e87b847
Silentbilly <rznstyle@gmail.com>
2017-04-30 22:57:23 +0300

    change function

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,19 +6,15 @@ const description = 'Is prime the integer?\n';
 
 const game = () => {
   const num = getRandomInt(0, 100);
-  const prime = () => {
-    if (num < 2) {
-      return false;
-    }
 
-    for (let i = 2; i < num / 2; i += 1) {
-      if (num % i === 0) {
-        return false;
-      }
+  const prime = () => {
+    if (num === 2) return true;
+    else if (num <= 1 || num % 2 === 0) return false;
+    for (let i = 3; i <= Math.sqrt(num); i += 2) {
+      if (num % i === 0) return false;
     }
     return true;
   };
-
   const isPrime = () => (prime(num) ? 'yes' : 'no');
 
   return cons(num, isPrime(num));
