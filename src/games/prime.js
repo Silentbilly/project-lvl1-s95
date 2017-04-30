8ef35a119f22fca472fa6f5193d6478e86cf8cd4
Silentbilly <rznstyle@gmail.com>
2017-04-30 21:32:21 +0300

    Update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -11,7 +11,7 @@ const game = () => {
       return false;
     }
 
-    for (let i = 2; i < num; i += 1) {
+    for (let i = 2; i < num / 2; i += 1) {
       if (num % i === 0) {
         return false;
       }
