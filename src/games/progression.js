6ef0925aa2ccae4c48f58e6e75553b5dde1fd50a
Silentbilly <rznstyle@gmail.com>
2017-04-30 11:26:35 +0300

    add game progression

---
+++ src/games/progression.js
@@ -0,0 +1,34 @@
+import { cons } from 'hexlet-pairs';
+import getRandomInt from '../common';
+import runGame from '../gameplay';
+
+const description = 'What number is missing in this progression?\n';
+
+const progression = () => {
+  const missingIndex = 5;
+  const limit = 9;
+  const step = getRandomInt(1, 9);
+  const arrayFillProgression = () => {
+    const arr = [];
+    arr[0] = getRandomInt(0, 9);
+    for (let i = 0; i < limit; i += 1) {
+      arr.push(arr[i] + step);
+    }
+    return arr;
+  };
+  const arrayWihoutElement = () => {
+    const newArr = arrayFillProgression();
+    newArr.splice(missingIndex, 1, '..');
+    return newArr;
+  };
+  const array = arrayWihoutElement();
+  const stepOfExpectedMissingValue = (array[missingIndex + 1] - array[missingIndex - 1]) / 2;
+  const expectedMissingValue = array[missingIndex - 1] + stepOfExpectedMissingValue;
+  return cons(array.join(' '), `${expectedMissingValue}`);
+};
+
+const game = () => progression();
+
+export default () => {
+  runGame(description, game);
+};
