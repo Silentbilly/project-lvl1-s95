dec8183b122266ebc4b78f070babd162f53d52e8
Silentbilly <rznstyle@gmail.com>
2017-05-01 11:28:52 +0500

    refactoring

--- src/games/even.js
+++ src/games/even.js
@@ -2,7 +2,7 @@ import { cons } from 'hexlet-pairs';
 import getRandomInt from '../common';
 import runGame from '../gameplay';
 
-const description = 'Answer "yes" if number even otherwise answer "no".\n';
+const description = 'Answer "yes" if number even otherwise answer "no".';
 
 const isEven = num => (num % 2 === 0 ? 'yes' : 'no');
 
