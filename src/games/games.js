0c18de42d1fae45e455a1e108b4223c552f03511
Silentbilly <rznstyle@gmail.com>
2017-05-01 11:26:05 +0500

    reactoring

--- src/games/games.js
+++ src/games/games.js
@@ -1,7 +1,5 @@
-import { makeQuestion } from '../common';
-
 export default () => {
   console.log('Welcome to the Brain Games!\n');
-  const name = makeQuestion('May I have your name?');
+  const name = readlineSync.question('May I have your name?');
   console.log(`Hello, ${name}!`);
 };
