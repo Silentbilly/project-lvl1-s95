8a7de426edab5486eeb41078d08c0375d458acdb
Silentbilly <rznstyle@gmail.com>
2017-05-01 11:35:32 +0500

    refactoring

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,12 +5,12 @@ import runGame from '../gameplay';
 const description = 'Is prime the integer?';
 
 const isPrime = () => {
-    if (num === 2) return true;
-    else if (num <= 1 || num % 2 === 0) return false;
-    for (let i = 3; i <= Math.sqrt(num); i += 2) {
-      if (num % i === 0) return false;
-    }
-    return true;
+  if (num === 2) return true;
+  else if (num <= 1 || num % 2 === 0) return false;
+  for (let i = 3; i <= Math.sqrt(num); i += 2) {
+    if (num % i === 0) return false;
+  }
+  return true;
 };
 
 const game = () => {
