aa3b40b459de9e028c4d611a17500e140e65e762
Silentbilly <rznstyle@gmail.com>
2017-05-01 11:31:03 +0500

    refactoring

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,20 +2,19 @@ import { cons } from 'hexlet-pairs';
 import getRandomInt from '../common';
 import runGame from '../gameplay';
 
-const description = 'Is prime the integer?\n';
+const description = 'Is prime the integer?';
 
-const game = () => {
-  const num = getRandomInt(0, 100);
-
-  const isPrime = () => {
+const isPrime = () => {
     if (num === 2) return true;
     else if (num <= 1 || num % 2 === 0) return false;
     for (let i = 3; i <= Math.sqrt(num); i += 2) {
       if (num % i === 0) return false;
     }
     return true;
-  };
+};
 
+const game = () => {
+  const num = getRandomInt(0, 100);
   return cons(num, isPrime(num) ? 'yes' : 'no');
 };
 
